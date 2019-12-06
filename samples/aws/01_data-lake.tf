fbbdac690f2000dae214c190a3c25d8ec49c4fc5
Aaron Steers <18150651+aaronsteers@users.noreply.github.com>
2019-12-06 00:29:23 -0800

    terraform format

--- samples/aws/01_data-lake.tf
+++ samples/aws/01_data-lake.tf
@@ -1,5 +1,5 @@
 module "data_late_on_aws" {
-  source = "../../catalog/data-lake-on-aws"
+  source      = "../../catalog/data-lake-on-aws"
   name_prefix = "${var.project_shortname}-Tableau-"
-  aws_region = var.aws_region
+  aws_region  = var.aws_region
 }
--- samples/aws/02_tableau-server.tf
+++ samples/aws/02_tableau-server.tf
@@ -1,8 +1,8 @@
 module "tablea_server_on_aws" {
-  source = "../../catalog/tableau-server-on-aws"
-  name_prefix = "${var.project_shortname}-Tableau-"
-  aws_region = var.aws_region
-  num_linux_instances = 1
+  source                = "../../catalog/tableau-server-on-aws"
+  name_prefix           = "${var.project_shortname}-Tableau-"
+  aws_region            = var.aws_region
+  num_linux_instances   = 1
   num_windows_instances = 0
-  c2_instance_type = "m4.4xlarge"
+  c2_instance_type      = "m4.4xlarge"
 }
