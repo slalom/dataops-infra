929428d50295123b4a04bd9476b357c0bb8387ef
Aaron Steers <18150651+aaronsteers@users.noreply.github.com>
2019-12-05 18:19:23 -0800

    format fix

--- modules/aws/ec2/variables.tf
+++ modules/aws/ec2/variables.tf
@@ -13,15 +13,15 @@ variable "ami_owner" { default = "amazon" }
 variable "ami_name_filter" { type = "string" }
 variable "is_windows" { default = false }
 variable "admin_ports" {
-    type    = "map"
-    default = {}
+  type    = "map"
+  default = {}
 }
 variable "files" {
-    default = []
-    description = "List of files to needed on the instance (e.g. 'http://url/to/remote/file', '/path/to/local/file', '/path/to/local/file:renamed')"
+  default     = []
+  description = "List of files to needed on the instance (e.g. 'http://url/to/remote/file', '/path/to/local/file', '/path/to/local/file:renamed')"
 }
 variable "app_ports" {
-    type    = "map"
-    description = "map of port descriptions to port numbers (e.g. 22) or ranges (e.g. '0:65535')"
-    default = {"SSH": "22"}
+  type        = "map"
+  description = "map of port descriptions to port numbers (e.g. 22) or ranges (e.g. '0:65535')"
+  default     = { "SSH" : "22" }
 }
