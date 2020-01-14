9227902516ea6d24ed1aa53d337a9215ddc21615
Aaron Steers <18150651+aaronsteers@users.noreply.github.com>
2020-01-14 13:29:24 -0800

    fix typo

--- samples/aws/01_data-lake.tf
+++ samples/aws/01_data-lake.tf
@@ -1,4 +1,4 @@
-module "data_late_on_aws" {
+module "data_lake_on_aws" {
   source      = "git::https://github.com/slalom-ggp/dataops-infra.git//catalog/data-lake-on-aws?ref=master"
   name_prefix = "${local.project_shortname}-Tableau-"
   aws_region  = local.aws_region
