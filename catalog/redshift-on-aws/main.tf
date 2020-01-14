411a310f67dd3239624d4866c4b8329b07de75d9
Aaron Steers <18150651+aaronsteers@users.noreply.github.com>
2020-01-14 15:30:06 -0800

    improved formatting

--- catalog/redshift-on-aws/main.tf
+++ catalog/redshift-on-aws/main.tf
@@ -7,21 +7,25 @@ resource "random_id" "random_pass" {
 resource "aws_redshift_cluster" "redshift" {
   cluster_identifier = "${lower(var.project_shortname)}-redshift"
   database_name      = "${lower(var.project_shortname)}db"
+
   master_username    = "rsadmin"
   master_password = (
     var.admin_password == null
     ? "${lower(substr(random_id.random_pass.hex, 0, 4))}${upper(substr(random_id.random_pass.hex, 4, 4))}"
     : var.admin_password
   )
+
   node_type       = var.node_type
-  cluster_type    = var.num_nodes > 1 ? "multi-node" : "single-node"
   number_of_nodes = var.num_nodes
+  cluster_type    = var.num_nodes > 1 ? "multi-node" : "single-node"
+
   kms_key_id      = var.kms_key_id
   elastic_ip      = var.elastic_ip
+  port            = var.jdbc_port
+
   logging {
     enable        = var.s3_logging_bucket == null ? false : true
     bucket_name   = var.s3_logging_bucket
     s3_key_prefix = var.s3_logging_path
   }
-  # port             = 5439
 }
--- samples/redshift-dw/02_redshift.tf
+++ samples/redshift-dw/02_redshift.tf
@@ -2,7 +2,19 @@ module "redshift_dw" {
   # source          = "git::https://github.com/slalom-ggp/dataops-infra.git//catalog/redshift-on-aws?ref=master"
   source            = "../../catalog/redshift-on-aws"
   project_shortname = local.project_shortname
-  # node_type         = "dc2.large"
-  # admin_password    = "asdfAS12"
-  # aws_region        = local.aws_region
+
+  # CONFIGURE HERE:
+
+
+  /*
+  # OPTIONALLY, COPY-PASTE ADDITIONAL SETTINGS FROM BELOW:
+
+  node_type         = "dc2.large"  # https://aws.amazon.com/redshift/pricing/
+  num_nodes         = 2
+
+  admin_password    = "asdfAS12"
+  aws_region        = local.aws_region
+  */
 }
+
+output "summary" { value = module.redshift_dw.summary }
