7b58a038ef0b00f1f3bc4376198ea6f7285dfd81
Aaron Steers <18150651+aaronsteers@users.noreply.github.com>
2020-01-16 13:03:07 -0800

    read-only aws role

--- catalog/aws-prereqs/main.tf
+++ catalog/aws-prereqs/main.tf
@@ -75,6 +75,26 @@ resource "local_file" "aws_credentials_file" {
   )
 }
 
+resource "aws_iam_role" "terraform-readonly" {
+  name = "terraform-readonly"
+  path = "/",
+  assume_role_policy = "${data.aws_iam_policy_document.assume-terraform-readonly-role-policy.json}"
+}
+
+resource "aws_iam_policy" "terraform-readonly" {
+  name = "terraform-readonly"
+  path = "/"
+  description = "Readonly policy for terraform planning"
+  policy = "${file("policies/terraform-readonly.json")}"
+}
+
+resource "aws_iam_policy_attachment" "terraform-readonly-attachment" {
+  name = "Terraform read-only attachment"
+  roles = ["${aws_iam_role.terraform-readonly.name}"]
+  policy_arn = "${aws_iam_policy.terraform-readonly.arn}"
+}
+
+
 # resource "aws_iam_user" "automation_user" {
 #   name = "${var.project_shortname}-automation-user"
 #   tags = {
