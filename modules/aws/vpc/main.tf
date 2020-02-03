d416307c5a6422b6aa719a49a02f57471eb8aa76
Aaron Steers <18150651+aaronsteers@users.noreply.github.com>
2020-02-03 11:30:08 -0800

    fixed naming convention

--- modules/aws/vpc/main.tf
+++ modules/aws/vpc/main.tf
@@ -18,14 +18,14 @@ data "aws_availability_zones" "az_list" {
   provider = aws.regional
 }
 
-resource "aws_eip" "myEIP" {
+resource "aws_eip" "eip" {
   tags = {
     Name    = "${var.name_prefix}EIP"
     project = local.project_shortname
   }
 }
 
-resource "aws_vpc" "myVPC" {
+resource "aws_vpc" "my_vpc" {
   cidr_block = "10.0.0.0/16"
   tags = {
     Name    = "${var.name_prefix}VPC"
@@ -38,7 +38,7 @@ resource "aws_subnet" "public_subnets" {
 
   availability_zone       = data.aws_availability_zones.az_list.names[count.index]
   cidr_block              = "10.0.${count.index + 2}.0/24"
-  vpc_id                  = aws_vpc.myVPC.id
+  vpc_id                  = aws_vpc.my_vpc.id
   map_public_ip_on_launch = true
   tags = {
     Name    = "${var.name_prefix}PublicSubnet-${count.index}"
@@ -51,23 +51,23 @@ resource "aws_subnet" "private_subnets" {
 
   availability_zone = data.aws_availability_zones.az_list.names[count.index]
   cidr_block        = "10.0.${count.index}.0/24"
-  vpc_id            = aws_vpc.myVPC.id
+  vpc_id            = aws_vpc.my_vpc.id
   tags = {
     Name    = "${var.name_prefix}PrivateSubnet-${count.index}"
     project = local.project_shortname
   }
 }
 
-resource "aws_internet_gateway" "myIGW" {
-  vpc_id = aws_vpc.myVPC.id
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = aws_vpc.my_vpc.id
   tags = {
     Name    = "${var.name_prefix}IGW"
     project = local.project_shortname
   }
 }
 
-resource "aws_nat_gateway" "myNATGateway" {
-  allocation_id = aws_eip.myEIP.id
+resource "aws_nat_gateway" "nat_gateway" {
+  allocation_id = aws_eip.eip.id
   subnet_id     = aws_subnet.public_subnets.0.id
   tags = {
     Name    = "${var.name_prefix}NAT"
@@ -75,42 +75,42 @@ resource "aws_nat_gateway" "myNATGateway" {
   }
 }
 
-resource "aws_route_table" "myPublicRT" {
-  vpc_id = aws_vpc.myVPC.id
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.my_vpc.id
   tags = {
     Name    = "${var.name_prefix}PublicRT"
     project = local.project_shortname
   }
 }
 
-resource "aws_route_table_association" "myPublicRTAssoc" {
+resource "aws_route_table_association" "public_rt_assoc" {
   count          = 2
-  route_table_id = aws_route_table.myPublicRT.id
+  route_table_id = aws_route_table.public_rt.id
   subnet_id      = aws_subnet.public_subnets.*.id[count.index]
 }
 
-resource "aws_route" "myIGWRoute" {
-  route_table_id         = aws_route_table.myPublicRT.id
-  gateway_id             = aws_internet_gateway.myIGW.id
+resource "aws_route" "igw_route" {
+  route_table_id         = aws_route_table.public_rt.id
+  gateway_id             = aws_internet_gateway.my_igw.id
   destination_cidr_block = "0.0.0.0/0"
 }
 
-resource "aws_route_table_association" "myPrivateRTAssoc" {
+resource "aws_route_table_association" "private_rt_assoc" {
   count          = 2
-  route_table_id = aws_route_table.myPrivateRT.id
+  route_table_id = aws_route_table.private_rt.id
   subnet_id      = aws_subnet.private_subnets.*.id[count.index]
 }
 
-resource "aws_route_table" "myPrivateRT" {
-  vpc_id = aws_vpc.myVPC.id
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.my_vpc.id
   tags = {
     Name    = "${var.name_prefix}PrivateRT"
     project = local.project_shortname
   }
 }
 
-resource "aws_route" "myNATRoute" {
-  route_table_id         = aws_route_table.myPrivateRT.id
-  nat_gateway_id         = aws_nat_gateway.myNATGateway.id
+resource "aws_route" "nat_route" {
+  route_table_id         = aws_route_table.private_rt.id
+  nat_gateway_id         = aws_nat_gateway.nat_gateway.id
   destination_cidr_block = "0.0.0.0/0"
 }
