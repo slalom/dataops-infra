089b66fc5f10acb7eb817db7e685c95015fd3133
Aaron Steers <18150651+aaronsteers@users.noreply.github.com>
2020-02-03 11:29:37 -0800

    improved output summaries

--- catalog/aws/airflow/outputs.tf
+++ catalog/aws/airflow/outputs.tf
@@ -1,4 +1,10 @@
 output "logging_url" { value = module.airflow_ecs_task.ecs_logging_url }
 # output "webui_endpoint" { value = "${module.airflow_ecs_task.public_ip_address}:8080"} 
 output "server_launch_cli" { value = module.airflow_ecs_task.ecs_runtask_cli }
-output "summary" { value = "" }
+output "summary" {
+  value = <<EOF
+Airflow Summary:
+  Launch command: ${module.airflow_ecs_task.ecs_runtask_cli}
+  Logging URL:    ${module.airflow_ecs_task.ecs_logging_url}
+EOF
+}
--- catalog/aws/dbt/outputs.tf
+++ catalog/aws/dbt/outputs.tf
@@ -1,1 +1,7 @@
-output "summary" { value = "" }
+output "summary" {
+  value = <<EOF
+DBT Summary:
+  Run command: ${module.ecs_task.ecs_runtask_cli}
+  Logging URL: ${module.ecs_task.ecs_logging_url}
+EOF
+}
