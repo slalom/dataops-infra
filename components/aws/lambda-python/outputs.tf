feb0fde285806729d76b353542b428db6faad0e4
Aaron Steers <18150651+aaronsteers@users.noreply.github.com>
2020-02-21 02:33:41 -0800

    bugfix

--- components/aws/lambda-python/outputs.tf
+++ components/aws/lambda-python/outputs.tf
@@ -2,5 +2,5 @@ output "build_temp_dir" {
   value = local.temp_build_folder
 }
 output "python_zip_size" {
-  value = format("%.2fMB", aws_lambda_function.python_lambda.source_code_size / 1000000)
+  value = format("%.2fMB", max(aws_lambda_function.python_lambda.*.source_code_size) / 1000000)
 }
