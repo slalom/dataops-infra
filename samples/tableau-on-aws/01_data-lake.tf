36e923815988e1099ebcc60dad37327dcde81163
Aaron Steers <18150651+aaronsteers@users.noreply.github.com>
2020-02-26 02:05:19 -0800

    wip env

--- samples/tableau-on-aws/01_data-lake.tf
+++ samples/tableau-on-aws/01_data-lake.tf
@@ -2,7 +2,7 @@ module "data_lake_on_aws" {
   # source      = "git::https://github.com/slalom-ggp/dataops-infra.git//catalog/aws/data-lake?ref=master"
   source      = "../../catalog/aws/data-lake"
   name_prefix = local.name_prefix
-  environment = var.environment
+  environment = module.env.environment
 
   # ADD OR MODIFY CONFIGURATION HERE:
 
