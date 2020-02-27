f940399d8847ab2eeee253b1acca6ccefafa2fe9
Aaron Steers <18150651+aaronsteers@users.noreply.github.com>
2020-02-26 16:06:13 -0800

    fix gradle path

--- components/aws/secrets-manager/outputs.tf
+++ components/aws/secrets-manager/outputs.tf
@@ -6,10 +6,7 @@ Secrets Summary:
  - Secret ARNs: ${coalesce(join(", ", aws_secretsmanager_secret.secrets.*.arn), "(empty)")}
 EOF
 }
-output "arn_list" {
-  value = aws_secretsmanager_secret.secrets.*.arn
-}
-output "secret_ids" {
+output "secrets_ids" {
   value = {
     for secret_id in var.secrets_names :
     secret_id => aws_secretsmanager_secret.secrets[secret_id].arn
