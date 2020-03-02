becbe8381183aeeb51dfabb1eb71d3ef135bacd1
Aaron Steers <18150651+aaronsteers@users.noreply.github.com>
2020-03-01 22:23:01 -0800

    updated env

--- samples/dbt-and-singer-on-aws/00_environment.tf
+++ samples/dbt-and-singer-on-aws/00_environment.tf
@@ -1,15 +1,11 @@
-# STANDARD TERRAFORM ENVIRONMENT DEFINITION
+# STANDARD ENVIRONMENT DEFINITION
 # NO NEED TO MODIFY THIS FILE
 
 data "local_file" "config_yml" { filename = "${path.module}/../infra-config.yml" }
-data "aws_caller_identity" "current" {}
-data "aws_availability_zones" "az_list" {}
-
 locals {
   config            = yamldecode(data.local_file.config_yml.content)
   secrets_folder    = "${path.module}/../.secrets"
   secrets_file_path = "${local.secrets_folder}/aws-secrets-manager-secrets.yml"
-  secrets_file      = fileexists(local.secrets_file_path) ? local.secrets_file_path : null
   project_shortname = local.config["project_shortname"]
   name_prefix       = "${local.project_shortname}-"
   aws_region        = local.config["aws_region"]
@@ -20,14 +16,13 @@ provider "aws" {
   version                 = "~> 2.10"
   region                  = local.aws_region
   shared_credentials_file = "${local.secrets_folder}/credentials"
-  profile                 = "${local.project_shortname}-terraform"
+  profile                 = "default"
 }
 
 module "env" {
-  # TODO: Revert to stable source
-  source         = "../../catalog/aws/environment"
+  source         = "../../dataops-infra/catalog/aws/environment"
   name_prefix    = local.name_prefix
   aws_region     = local.aws_region
-  resource_tags  = local.config["resource_tags"]
+  resource_tags  = local.resource_tags
   secrets_folder = local.secrets_folder
 }
