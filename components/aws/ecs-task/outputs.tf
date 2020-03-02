f89fdb01f289e917355fa947fd212841b4e96ea4
Aaron Steers <18150651+aaronsteers@users.noreply.github.com>
2020-03-02 13:25:14 -0800

    Fixes and improvements from template Speedrun (#57)
    * fixes from template speedrun
    * updated env
    * ecs and singer fixes
    * fix target secret mapping
    * update samples
    * don't fail sibling ci/cd tests
    * fixed output errors
    * tf vers 0.12.21 for ci/cd

--- components/aws/ecs-task/outputs.tf
+++ components/aws/ecs-task/outputs.tf
@@ -7,5 +7,9 @@ output "ecs_task_name" { value = "${aws_ecs_task_definition.ecs_task.family}" }
 output "ecs_runtask_cli" {
   value = "aws ecs run-task --task-definition ${aws_ecs_task_definition.ecs_task.family} --cluster ${var.ecs_cluster_name} --launch-type ${local.launch_type} --region ${var.environment.aws_region} ${var.use_fargate ? "--network-configuration awsvpcConfiguration={subnets=[${element(var.environment.public_subnets, 0)}],securityGroups=[${aws_security_group.ecs_tasks_sg.id}],assignPublicIp=ENABLED}" : ""}"
 }
+output "ecs_checklogs_cli" {
+  value = "aws logs get-log-events --log-group-name ${aws_cloudwatch_log_group.cw_log_group.name} --limit 100"
+  #  optional: --log-stream-name streamName
+}
 output "load_balancer_arn" { value = var.use_load_balancer ? aws_lb.alb[0].arn : null }
 output "load_balancer_dns" { value = var.use_load_balancer ? aws_lb.alb[0].dns_name : null }
--- samples/dbt-and-singer-on-aws/00_environment.tf
+++ samples/dbt-and-singer-on-aws/00_environment.tf
@@ -1,31 +1,29 @@
-# STANDARD TERRAFORM ENVIRONMENT DEFINITION
+# STANDARD ENVIRONMENT DEFINITION
 # NO NEED TO MODIFY THIS FILE
 
 data "local_file" "config_yml" { filename = "${path.module}/../infra-config.yml" }
-data "aws_caller_identity" "current" {}
-data "aws_availability_zones" "az_list" {}
-
 locals {
   config            = yamldecode(data.local_file.config_yml.content)
-  secrets_file_path = "${path.module}/../.secrets/aws-secrets-manager-secrets.yml"
-  secrets_file      = fileexists(local.secrets_file_path) ? local.secrets_file_path : null
+  secrets_folder    = "${path.module}/../../.secrets"
+  secrets_file_path = "${local.secrets_folder}/aws-secrets-manager-secrets.yml"
   project_shortname = local.config["project_shortname"]
   name_prefix       = "${local.project_shortname}-"
   aws_region        = local.config["aws_region"]
-  resource_tags     = local.config["project_tags"]
+  resource_tags     = local.config["resource_tags"]
 }
 
 provider "aws" {
   version                 = "~> 2.10"
   region                  = local.aws_region
-  shared_credentials_file = "../../.secrets/credentials"
-  profile                 = "${local.project_shortname}-terraform"
+  shared_credentials_file = "${local.secrets_folder}/credentials"
+  profile                 = "default"
 }
 
+output "env_summary" { value = module.env.summary }
 module "env" {
-  # TODO: Revert to stable source
-  source        = "../../catalog/aws/environment"
-  name_prefix   = local.name_prefix
-  aws_region    = local.aws_region
-  resource_tags = local.config["project_tags"]
+  source         = "../../catalog/aws/environment"
+  name_prefix    = local.name_prefix
+  aws_region     = local.aws_region
+  resource_tags  = local.resource_tags
+  secrets_folder = local.secrets_folder
 }
