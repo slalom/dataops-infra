2373964946af30ca046f9a61b195c7895f970351
Aaron Steers <18150651+aaronsteers@users.noreply.github.com>
2020-03-03 15:49:01 -0800

    simplified secrets manager usage

--- components/aws/secrets-manager/outputs.tf
+++ components/aws/secrets-manager/outputs.tf
@@ -1,20 +1,19 @@
-# TODO:  BUGFIX:'summary' and 'arn_list' appear to be returning null
 output "summary" {
   value = <<EOF
 
 
 Secrets Summary:
- - Secret ARNs: ${"\n\t"}${coalesce(join("\n\t",
+ - Secret ARNs:
+ ${"\t"}${
+   coalesce(join("\n\t",
   [
-    for secret_name in local.secrets_names :
-    "${secret_name} => ${aws_secretsmanager_secret.secrets[secret_name].id}"
+    for name, id in local.merged_secrets_map :
+      "${name} => ${id}"
   ]
-), "(empty)")}
+  ), "(none)")
+}
 EOF
 }
 output "secrets_ids" {
-  value = {
-    for secret_name in local.secrets_names :
-    secret_name => aws_secretsmanager_secret.secrets[secret_name].id
-  }
+  value = local.merged_secrets_map
 }
--- components/aws/secrets-manager/variables.tf
+++ components/aws/secrets-manager/variables.tf
@@ -4,13 +4,18 @@ variable "resource_tags" {
   default = {}
 }
 
-variable "secrets_file" {
-  description = "Input file (yaml or json) which contains the secrets to be uploaded."
-  default     = null
-}
+# variable "secrets_file" {
+#   description = "Input file (yaml or json) which contains the secrets to be uploaded."
+#   default     = null
+# }
 
-variable "secrets_file_map" {
-  description = "A map of secret names to their property names in the secrets file."
+variable "secrets_map" {
+  description = <<EOF
+A map between secret names and their locations.
+The location can be:
+ - ID of an existing Secrets Manager secret (`arn:aws:secretsmanager:...`)
+ - String with the local secrets file name and property names separated by ':'."
+EOF
   type        = map(string)
   default     = {}
 }
