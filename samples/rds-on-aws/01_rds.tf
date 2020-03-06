6a705c614ff5856266f36c271c961f09f76a3dfd
mithra-ramesh <mithra.ramesh@slalom.com>
2020-03-05 19:29:15 -0500

    removed unnecessary variables

--- samples/rds-on-aws/01_rds.tf
+++ samples/rds-on-aws/01_rds.tf
@@ -20,17 +20,5 @@ module "rds_mysql" {
   skip_final_snapshot = true
 
 
-  /*
-  # OPTIONALLY, COPY-PASTE ADDITIONAL SETTINGS FROM BELOW:
-
-  identifier      = "mysql-db"
-  node_type       = "dc2.large"  #  "dc2.large" is smallest, costs ~$200/mo: https://aws.amazon.com/redshift/pricing/
-  num_nodes       = 1
-
-  admin_password  = "asdfAS12"
-  aws_region      = local.aws_region
-  */
-}
-
 output "summary" { value = module.rds_mysql.summary }
 
