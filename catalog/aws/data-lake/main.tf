73cacd01c45710d3ff4a9f858017f103ec1d4978
Aaron Steers <18150651+aaronsteers@users.noreply.github.com>
2020-03-07 14:42:18 -0800

    Updates and fixes from usage tests (#63)
    * remove obsolete `catalog/aws/prereqs`
    * cleanup aws creds file references, pass along to vpc module
    * add back old `regional` provider alias (avoid breaking downstream wip branches)
    * optional `data_bucket_override` variable in `catalog/aws/data_lake`
    * avoid name collisions and fix secrets access in `ecs-task` module
    * remove `lambda_python_zip_size`

--- catalog/aws/data-lake/main.tf
+++ catalog/aws/data-lake/main.tf
@@ -1,13 +1,20 @@
-resource "random_id" "suffix" {
-  byte_length = 2
+resource "random_id" "suffix" { byte_length = 2 }
+
+data aws_s3_bucket "data_bucket_override" {
+  count  = var.data_bucket_override != null ? 1 : 0
+  bucket = var.data_bucket_override
 }
 
 locals {
-  s3_path_to_lambda_zip = "s3://${aws_s3_bucket.s3_metadata_bucket.bucket}/code/lambda/${var.name_prefix}lambda.zip"
+  s3_path_to_lambda_zip = "s3://${aws_s3_bucket.s3_metadata_bucket.id}/code/lambda/${var.name_prefix}lambda.zip"
   random_bucket_suffix  = lower(random_id.suffix.hex)
+  data_bucket_name = (
+    var.data_bucket_override != null ? data.aws_s3_bucket.data_bucket_override[0].id : aws_s3_bucket.s3_data_bucket[0].id
+  )
 }
 
 resource "aws_s3_bucket" "s3_data_bucket" {
+  count  = var.data_bucket_override == null ? 1 : 0
   bucket = "${lower(var.name_prefix)}data-${local.random_bucket_suffix}"
   acl    = "private"
   tags   = var.resource_tags
@@ -53,7 +60,7 @@ module "triggered_lambda" {
   source                = "../../../components/aws/lambda-python"
   name_prefix           = var.name_prefix
   environment           = var.environment
-  s3_trigger_bucket     = aws_s3_bucket.s3_data_bucket.bucket
+  s3_trigger_bucket     = local.data_bucket_name
   s3_triggers           = var.s3_triggers
   lambda_source_folder  = var.lambda_python_source
   s3_path_to_lambda_zip = local.s3_path_to_lambda_zip
--- components/aws/vpc/main.tf
+++ components/aws/vpc/main.tf
@@ -8,8 +8,17 @@ locals {
   aws_region        = coalesce(var.aws_region, data.aws_region.current.name)
 }
 
+#TODO: Remove this old version after all legacy dependencies have cleared.
 provider "aws" {
-  alias                   = "region_lookup"
+  alias                   = "regional" # used for region-specific AZ lookup
+  version                 = "~> 2.10"
+  region                  = local.aws_region
+  shared_credentials_file = var.aws_credentials_file
+  profile                 = var.aws_profile
+}
+
+provider "aws" {
+  alias                   = "region_lookup" # used for region-specific AZ lookup
   version                 = "~> 2.10"
   region                  = local.aws_region
   shared_credentials_file = var.aws_credentials_file
