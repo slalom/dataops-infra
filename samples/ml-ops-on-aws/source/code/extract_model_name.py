e766cc25d90b8802267bd1c8dddf47bd28cbcf4c
Aaron Steers <18150651+aaronsteers@users.noreply.github.com>
2020-03-09 16:52:09 -0700

    replace zip files with py

---
+++ samples/ml-ops-on-aws/source/code/extract_model_name.py
@@ -0,0 +1,8 @@
+def lambda_handler(event, context):
+  if('ModelArn' in event):
+    model_name = event['ModelArn'].split('/')[-1]
+  else:
+    model_name = event['EndpointConfigArn'].split('/')[-1]
+  return {
+      'modelName': model_name
+     }
---
+++ samples/ml-ops-on-aws/source/code/extract_model_path.py
@@ -0,0 +1,6 @@
+def lambda_handler(event, context):
+  print(event)
+  return {
+      'bestTrainingJobName': event['BestTrainingJob']['TrainingJobName'],
+      'modelDataUrl': event['TrainingJobDefinition']['OutputDataConfig']['S3OutputPath']+'/'+event['BestTrainingJob']['TrainingJobName']+'/output/model.tar.gz'
+     }
