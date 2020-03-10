7f31976acdb8f5169d0bad12361f1cc47475190c
Aaron Steers <18150651+aaronsteers@users.noreply.github.com>
2020-03-09 23:23:05 -0700

    import from PPL module

---
+++ catalog/aws/data-lake-users/outputs.tf
@@ -0,0 +1,39 @@
+output "aws_secret_secret_access_keys" {
+  description = "Mapping of user IDs to their secret access keys (encrypted)."
+  value = {
+    for user in var.users :
+    user => aws_iam_access_key.user_keys[user].encrypted_secret
+  }
+}
+
+output "summary" {
+  description = <<EOF
+Standard Output. Human-readable summary of what was created
+by the module and (when applicable) how to access those
+resources.
+EOF
+  value = <<EOF
+Data Lake Users Summary:
+
+ - ${
+  try(
+    join("\n\n - ", [
+      for user in var.users :
+      "${user}:\n${join("\n", [
+        "   - AWS Access Key ID:    ${try(aws_iam_access_key.user_keys[user].id, "(error)")}",
+        "   - Secret Key Retrieval: keybase pgp decrypt --infile ${try(local_file.encrypted_secret_key_files[user].filename, "(error)")}"
+      ])}"
+    ])
+    )}
+
+Data Lake Groups:
+ - ${try(
+    join("\n\n - ", [
+      for group in local.group_names :
+      "${group}:\n${join("\n", [
+        "   - KMS Key Alias: ${aws_kms_alias.group_kms_key_alias[group].id}"
+      ])}"
+    ])
+)}
+EOF
+}
