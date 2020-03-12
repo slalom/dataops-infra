7ed286a1401d2aca17bd41a5a7a0eb4483b6143e
Aaron Steers <18150651+aaronsteers@users.noreply.github.com>
2020-03-11 22:22:50 -0700

    fix dupe 'resource_tags'

--- components/aws/vpc/variables.tf
+++ components/aws/vpc/variables.tf
@@ -26,10 +26,6 @@ variable "resource_tags" {
 ########################################
 
 variable "aws_region" { default = null }
-variable "resource_tags" {
-  type    = map
-  default = {}
-}
 variable "disabled" {
   description = "As a workaround for unsupported 'count' feature in terraform modules, this switch can be used to disable the module entirely."
   default     = false
