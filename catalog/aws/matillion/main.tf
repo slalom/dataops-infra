0433fc9ac228f6bb77df326ede9313ad8cf1b591
Aaron Steers <18150651+aaronsteers@users.noreply.github.com>
2020-03-16 17:35:21 -0700

    terraform validate passes

--- catalog/aws/matillion/main.tf
+++ catalog/aws/matillion/main.tf
@@ -14,28 +14,19 @@
 locals {
   name_prefix = "${var.name_prefix}Matillion-"
   admin_cidr  = var.admin_cidr
-  app_cidr    = length(var.app_cidr) == 0 ? local.admin_cidr : var.app_cidr
-  admin_ports = combine(
-    var.allow_http ? { "HTTP" : "80" } : {},
-    var.allow_https ? { "HTTPS" : "443" } : {}
+  admin_ports = merge(
+    { "HTTPS" : var.https_port },
+    var.allow_http ? { "HTTP" : var.http_port } : {},
   )
-
-  # # TODO: Delete or reuse:
-  #   ec2_files = flatten([
-  #     fileset(path.module, "resources/win/*"),
-  #     fileset(path.module, "resources/*"),
-  #     ["${var.registration_file}:registration.json"]
-  #   ])
-  #   ssh_key_dir              = pathexpand("~/.ssh")
-  #   ssh_public_key_filepath  = "${local.ssh_key_dir}/${lower(local.name_prefix)}prod-ec2keypair.pub"
-  #   ssh_private_key_filepath = "${local.ssh_key_dir}/${lower(local.name_prefix)}prod-ec2keypair.pem"
+  ssh_key_dir              = pathexpand("~/.ssh")
+  ssh_public_key_filepath  = "${local.ssh_key_dir}/${lower(local.name_prefix)}prod-ec2keypair.pub"
+  ssh_private_key_filepath = "${local.ssh_key_dir}/${lower(local.name_prefix)}prod-ec2keypair.pem"
 }
 
-# TODO: Delete or reuse:
-# resource "aws_key_pair" "mykey" {
-#   key_name   = "${local.name_prefix}ec2-keypair"
-#   public_key = file(local.ssh_public_key_filepath)
-# }
+resource "aws_key_pair" "mykey" {
+  key_name   = "${local.name_prefix}ec2-keypair"
+  public_key = file(local.ssh_public_key_filepath)
+}
 
 module "matillion_ec2" {
   source        = "../../../components/aws/ec2"
@@ -43,12 +34,13 @@ module "matillion_ec2" {
   environment   = var.environment
   resource_tags = var.resource_tags
 
-  num_instances       = 1
-  instance_type       = var.ec2_instance_type
-  instance_storage_gb = var.ec2_instance_storage_gb
-  ami_id              = local.ami_per_region[var.environment.aws_region]
-  admin_ports         = local.admin_ports
-  app_ports           = {}
-  #   ssh_key_name             = aws_key_pair.mykey.key_name
-  #   ssh_private_key_filepath = local.ssh_private_key_filepath
+  num_instances            = 1
+  instance_type            = var.ec2_instance_type
+  instance_storage_gb      = var.ec2_instance_storage_gb
+  admin_ports              = local.admin_ports
+  app_ports                = {}
+  ssh_key_name             = aws_key_pair.mykey.key_name
+  ssh_private_key_filepath = local.ssh_private_key_filepath
+  ami_owner                = "679593333241"
+  ami_name_filter          = "*matillion-etl-for-${var.warehouse_type}*${var.matillion_version}*"
 }
--- catalog/aws/matillion/variables.tf
+++ catalog/aws/matillion/variables.tf
@@ -1,3 +1,29 @@
+##############################################
+### Standard variables for all AWS modules ###
+##############################################
+
+variable "name_prefix" {
+  description = "Standard `name_prefix` module input."
+  type        = string
+}
+variable "environment" {
+  description = "Standard `environment` module input."
+  type = object({
+    vpc_id          = string
+    aws_region      = string
+    public_subnets  = list(string)
+    private_subnets = list(string)
+  })
+}
+variable "resource_tags" {
+  description = "Standard `resource_tags` module input."
+  type        = map(string)
+}
+
+########################################
+### Custom variables for this module ###
+########################################
+
 variable "admin_cidr" { default = [] }
 variable "ec2_instance_type" {
   description = <<EOF
@@ -5,7 +31,14 @@ The instance type for the Matillion server.
 EOF
   default     = "m4.4xlarge"
 }
-
+variable "warehouse_type" {
+  description = "Supported options are 'snowflake' or 'redshift'"
+  default     = "snowflake"
+}
+variable "matillion_version" {
+  description = "The version of Matillion to install. To determine the latest available version, see the AWS marketplace page [here](https://aws.amazon.com/marketplace/pp/B073GRSSZD?ref_=srh_res_product_title)."
+  default     = "1.42"
+}
 variable "scheduled_timezone" {
   description = "The timezone to use when calculating scheduled uptime. Supported timezones: PST, EST, UTC"
   default     = "PST"
@@ -31,7 +64,6 @@ EOF
 }
 variable "ec2_instance_storage_gb" { default = 100 }
 variable "allow_http" { default = false }
-variable "allow_https" { default = true }
 variable "http_port" { default = 80 }
 variable "https_port" { default = 443 }
 variable "create_https_ssl_cert" { default = true }
