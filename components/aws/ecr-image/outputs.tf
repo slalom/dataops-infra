58872f9183a86fd3e4e7acd0e9e32265c51a1f89
jacksandom <jack.sandom@slalom.com>
2020-04-30 15:52:15 +0100

    Fix outputs when ecr-image repo is disabled

--- components/aws/ecr-image/outputs.tf
+++ components/aws/ecr-image/outputs.tf
@@ -1,16 +1,16 @@
 output "ecr_repo_arn" {
   description = "The unique ID (ARN) of the ECR repo."
-  value       = "${aws_ecr_repository.ecr_repo[0].arn}"
+  value       = var.is_disabled ? "" : "${aws_ecr_repository.ecr_repo[0].arn}"
 }
 output "ecr_repo_root" {
   description = "The path to the ECR repo, excluding image name."
-  value       = "${dirname(aws_ecr_repository.ecr_repo[0].repository_url)}"
+  value       = var.is_disabled ? "" : "${dirname(aws_ecr_repository.ecr_repo[0].repository_url)}"
 }
 output "ecr_image_url" {
   description = "The full path to the ECR image, including image name."
-  value       = "${aws_ecr_repository.ecr_repo[0].repository_url}"
+  value       = var.is_disabled ? "" : "${aws_ecr_repository.ecr_repo[0].repository_url}"
 }
 output "ecr_image_url_and_tag" {
   description = "The full path to the ECR image, including image name and tag."
-  value       = "${aws_ecr_repository.ecr_repo[0].repository_url}:${var.tag}"
+  value       = var.is_disabled ? "" : "${aws_ecr_repository.ecr_repo[0].repository_url}:${var.tag}"
 }
