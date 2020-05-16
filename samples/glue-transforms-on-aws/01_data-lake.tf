1d9c5c3bec1e816792e8adf4ab6e7b7103a16441
jacksandom <jack.sandom@slalom.com>
2020-05-16 15:53:56 +0100

    Fix Glue job

--- samples/glue-transforms-on-aws/01_data-lake.tf
+++ samples/glue-transforms-on-aws/01_data-lake.tf
@@ -5,3 +5,13 @@ module "data_lake" {
   environment   = module.env.environment
   resource_tags = local.resource_tags
 }
+
+resource "aws_s3_bucket_object" "raw_data" {
+  bucket = module.data_lake.s3_data_bucket
+  key    = "RAW/County_Population.csv"
+  source = "data/County_Population.csv"
+
+  etag = filemd5(
+    "data/County_Population.csv",
+  )
+}
--- samples/glue-transforms-on-aws/python/transform.py
+++ samples/glue-transforms-on-aws/python/transform.py
@@ -1,6 +1,6 @@
 # PySpark Script for AWS Glue ETL
 
-#setup environment
+# setup environment
 import sys
 from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
@@ -17,21 +17,20 @@ args = getResolvedOptions(sys.argv,
 bucket = args['S3_DATA_BUCKET']
 
 sc = SparkContext()
-glue_context = GlueContext(sc)
 sqlContext = SQLContext(sc)
 spark = sqlContext.sparkSession
 
 # create dynamic frame from raw data stored on s3
-inputPop = glueContext.create_dynamic_frame.from_options(connection_type="s3", connection_options={"path": "s3a://covid19-lake/static-datasets/csv/CountyPopulation/County_Population.csv"}, format = "csv", format_options={}, transformation_ctx="")
-
-# create dataframe for each table
-inputPop_df = inputPop.toDF()
+inputPop_df = spark.read.csv(
+    "s3://{}/RAW/County_Population.csv".format(bucket), inferSchema=True, header=True)
 
 # create table to query from each dataframe
 inputPop_df.createOrReplaceTempView("inputPop_table")
 
 # write SQL statements to clean the data
-inputPop_clean = spark.sql("SELECT id2 as fips, County as county_name, State as state_name, Population Estimate 2018 as est_population FROM inputPop_table WHERE upper(state) = 'COLORADO'")
+inputPop_clean = spark.sql(
+    "SELECT id2 as fips, County as county_name, State as state_name, `Population Estimate 2018` as est_population FROM inputPop_table WHERE upper(State) = 'COLORADO'")
 
 # write out the files to curated folder
-inputPop_clean.write.mode("overwrite").parquet("s3://{}/CURATED/County_Population_CURATED.csv".format(bucket))
+inputPop_clean.write.option("header", "true").mode("overwrite").csv(
+    "s3://{}/CURATED/County_Population_CURATED".format(bucket))
