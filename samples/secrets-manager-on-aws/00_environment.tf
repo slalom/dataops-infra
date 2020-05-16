df217360c76ed0d800135b2da2b6c1ed459b2fc4
Aaron Steers <18150651+aaronsteers@users.noreply.github.com>
2020-05-16 16:12:04 -0700

    update environment samples

--- samples/secrets-manager-on-aws/00_environment.tf
+++ samples/secrets-manager-on-aws/00_environment.tf
@@ -1,29 +1,40 @@
-# STANDARD ENVIRONMENT DEFINITION
-# NO NEED TO MODIFY THIS FILE
+################################
+### CONFIGURE REQUIRED PATHS ###
+################################
 
-data "local_file" "config_yml" { filename = "${path.module}/../infra-config.yml" }
+locals {
+  root                 = "../.."                       # Path to root of repo
+  yaml_config_path     = "../infra-config.yml"         # Required settings
+  secrets_folder       = "../.secrets"                 # Default secrets location
+  aws_credentials_file = "../.secrets/aws-credentials" # AWS Credentials
+}
+
+############################################
+### STANDARD ENVIRONMENT DEFINITION      ###
+### NO NEED TO MODIFY REMAINDER OF FILE  ###
+############################################
+
+data "local_file" "config_yml" { filename = local.yaml_config_path }
 locals {
   config            = yamldecode(data.local_file.config_yml.content)
-  secrets_folder    = "${path.module}/../../.secrets"
-  secrets_file_path = "${local.secrets_folder}/aws-secrets-manager-secrets.yml"
   project_shortname = local.config["project_shortname"]
-  name_prefix       = "${local.project_shortname}-"
   aws_region        = local.config["aws_region"]
   resource_tags     = local.config["resource_tags"]
+  name_prefix       = "${local.project_shortname}-"
 }
 
 provider "aws" {
   version                 = "~> 2.10"
   region                  = local.aws_region
-  shared_credentials_file = "${local.secrets_folder}/aws-credentials"
+  shared_credentials_file = local.aws_credentials_file
   profile                 = "default"
 }
 
 output "env_summary" { value = module.env.summary }
 module "env" {
-  source         = "../../catalog/aws/environment"
-  name_prefix    = local.name_prefix
-  aws_region     = local.aws_region
-  resource_tags  = local.resource_tags
-  secrets_folder = local.secrets_folder
+  source               = "../../catalog/aws/environment"
+  name_prefix          = local.name_prefix
+  aws_region           = local.aws_region
+  aws_credentials_file = local.aws_credentials_file
+  resource_tags        = local.resource_tags
 }
