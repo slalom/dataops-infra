86c5053c2fe5cc229a91aab316b44722aa5fccdc
Aaron Steers <18150651+aaronsteers@users.noreply.github.com>
2020-05-16 16:48:51 -0700

    fix rename: glue_job_spark_flag

--- catalog/aws/ml-ops/glue-job.tf
+++ catalog/aws/ml-ops/glue-job.tf
@@ -4,7 +4,7 @@ module "glue_job" {
   environment   = var.environment
   resource_tags = var.resource_tags
 
-  with_spark                 = var.with_spark
+  with_spark                 = var.glue_job_spark_flag
   s3_script_bucket_name      = aws_s3_bucket.source_repository.id
   s3_source_bucket_name      = var.feature_store_override != null ? data.aws_s3_bucket.feature_store_override[0].id : aws_s3_bucket.feature_store[0].id
   s3_destination_bucket_name = aws_s3_bucket.extracts_store.id
