26207ea18928698c531f3cbbeded34dbdcbe83f2
Aaron Steers <18150651+aaronsteers@users.noreply.github.com>
2020-06-09 22:25:25 -0700

    uncomment readonly buckets

--- catalog/aws/singer-taps/step-function.tf
+++ catalog/aws/singer-taps/step-function.tf
@@ -4,8 +4,8 @@ module "step_function" {
   environment   = var.environment
   resource_tags = var.resource_tags
 
-  writeable_buckets = []
-  # readonly_buckets         = []
+  writeable_buckets        = []
+  readonly_buckets         = []
   state_machine_definition = ""
   lambda_functions         = {}
   ecs_tasks                = {}
