179d815a3be363dfcb645ddfa9b6edc3ae759433
chanheiy <30609223+chanheiy@users.noreply.github.com>
2020-07-27 11:53:16 -0400

    updated ml-ops-img

---
+++ catalog/aws/ml-ops-img/main.tf
@@ -0,0 +1,322 @@
+/*
+* This module automates MLOps tasks associated with training Machine Learning models.
+*
+* The module leverages Step Functions and Lambda functions as needed. The state machine
+* executes hyperparameter tuning, training, and deployments as needed. Deployment options
+* supported are Sagemaker endpoints and/or batch inference.
+*/
+
+
+locals {
+  # State machine input for creating or updating an inference endpoint
+  endpoint = <<EOF
+"Create Model Endpoint Config": {
+    "Resource": "arn:aws:states:::sagemaker:createEndpointConfig",
+    "Parameters": {
+      "EndpointConfigName.$": "$.modelName",
+      "ProductionVariants": [
+        {
+          "InitialInstanceCount": ${var.endpoint_instance_count},
+          "InstanceType": "${var.endpoint_instance_type}",
+          "ModelName.$": "$.modelName",
+          "VariantName": "AllTraffic"
+        }
+      ]
+    },
+    "Type": "Task",
+    "Next": "Check Endpoint Exists"
+  },
+  "Check Endpoint Exists": {
+    "Resource": "${module.lambda_functions.function_ids["CheckEndpointExists"]}",
+    "Parameters": {
+      "EndpointConfigArn.$": "$.EndpointConfigArn",
+      "EndpointName": "${var.endpoint_name}"
+    },
+    "Type": "Task",
+    "Next": "Create or Update Endpoint"
+  },
+  "Create or Update Endpoint": {
+    "Type": "Choice",
+    "Choices": [
+      {
+        "Variable": "$['CreateOrUpdate']",
+        "StringEquals": "Update",
+        "Next": "Update Existing Model Endpoint"
+      }
+    ],
+    "Default": "Create New Model Endpoint"
+  },
+  "Create New Model Endpoint": {
+    "Resource": "arn:aws:states:::sagemaker:createEndpoint",
+    "Parameters": {
+      "EndpointConfigName.$": "$.endpointConfig",
+      "EndpointName.$": "$.endpointName"
+    },
+    "Type": "Task",
+    "End": true
+  },
+  "Update Existing Model Endpoint": {
+    "Resource": "arn:aws:states:::sagemaker:updateEndpoint",
+    "Parameters": {
+      "EndpointConfigName.$": "$.endpointConfig",
+      "EndpointName.$": "$.endpointName"
+    },
+    "Type": "Task",
+    "End": true
+  }
+EOF
+  # State machine input for batch transformation
+  batch_transform = <<EOF
+"Batch Transform": {
+    "Type": "Task",
+    "Resource": "arn:aws:states:::sagemaker:createTransformJob.sync",
+    "Parameters": {
+      "ModelName.$": "$.modelName",
+      "TransformInput": {
+        "CompressionType": "None",
+        "ContentType": "text/csv",
+        "DataSource": {
+          "S3DataSource": {
+            "S3DataType": "S3Prefix",
+            "S3Uri": "s3://${aws_s3_bucket.data_store.id}/input_data/score/score.csv"
+          }
+        }
+      },
+      "TransformOutput": {
+        "S3OutputPath": "s3://${aws_s3_bucket.data_store.id}/batch-transform-output"
+      },
+      "TransformResources": {
+        "InstanceCount": "${var.batch_transform_instance_count}",
+        "InstanceType": "${var.batch_transform_instance_type}"
+      },
+      "TransformJobName.$": "$.modelName"
+    },
+    "Next": "Rename Batch Transform Output"
+    },
+"Rename Batch Transform Output": {
+  "Type": "Task",
+  "Resource": "${module.lambda_functions.function_ids["RenameBatchOutput"]}",
+  "Parameters": {
+    "Payload": {
+      "BucketName": "${aws_s3_bucket.data_store.id}",
+      "Path": "batch-transform-output"
+    }
+  },
+  "Next": "Run Glue Crawler"
+    },
+    "Run Glue Crawler": {
+      "Type": "Task",
+      "Resource": "${module.lambda_functions.function_ids["RunGlueCrawler"]}",
+      "Parameters": {
+        "Payload": {
+          "CrawlerName": "${module.glue_crawler.glue_crawler_name}"
+        }
+      },
+      "End": true
+    }
+EOF
+}
+
+module "ecr_image" {
+  source               = "../../../components/aws/ecr-image"
+  name_prefix          = var.name_prefix
+  environment          = var.environment
+  resource_tags        = var.resource_tags
+  aws_credentials_file = var.aws_credentials_file
+
+  repository_name   = "img-recog-sample-image"
+  source_image_path = "source/containers/ml-ops-byo-resnet18/resnet18"
+  tag               = "latest"
+}
+
+module "postgres" {
+  source        = "../../../catalog/aws/postgres"
+  name_prefix   = var.name_prefix
+  environment   = var.environment
+  resource_tags = var.resource_tags
+
+  postgres_version = "11"
+  database_name    = "ml_ops_img_reg_db"
+
+  admin_username = "pgadmin"
+  admin_password = "1234asdf"
+
+}
+
+module "step-functions" {
+  #source                  = "git::https://github.com/slalom-ggp/dataops-infra.git//catalog/aws/data-lake?ref=main"
+  source        = "../../../components/aws/step-functions"
+  name_prefix   = var.name_prefix
+  environment   = var.environment
+  resource_tags = var.resource_tags
+
+  lambda_functions = module.lambda_functions.function_ids
+  writeable_buckets = [
+    var.feature_store_override != null ? data.aws_s3_bucket.feature_store_override[0].id : aws_s3_bucket.feature_store[0].id,
+    aws_s3_bucket.data_store.id,
+    aws_s3_bucket.model_store.id
+  ]
+
+  state_machine_definition = <<EOF
+{
+ "StartAt": "Glue Data Transformation",
+  "States": {
+    "Glue Data Transformation": {
+      "Type": "Task",
+      "Resource": "arn:aws:states:::glue:startJobRun.sync",
+      "Parameters": {
+        "JobName": "${module.glue_job.glue_job_name}",
+        "Arguments": {
+          "--extra-py-files": "s3://${aws_s3_bucket.source_repository.id}/glue/python/pandasmodule-0.1-py3-none-any.whl",
+          "--S3_SOURCE": "${var.feature_store_override != null ? data.aws_s3_bucket.feature_store_override[0].id : aws_s3_bucket.feature_store[0].id}",
+          "--S3_DEST": "${aws_s3_bucket.data_store.id}",
+          "--TRAIN_KEY": "input_data/train",
+          "--VALIDATION_KEY": "input_data/validate",
+          "--SCORE_KEY": "input_data/score",
+          "--INFERENCE_TYPE": "${var.endpoint_or_batch_transform == "Create Model Endpoint Config" ? "endpoint" : "batch"}"
+        }
+      },
+      "Next": "Generate Unique Job Name"
+    },
+    "Generate Unique Job Name": {
+      "Resource": "${module.lambda_functions.function_ids["UniqueJobName"]}",
+      "Parameters": {
+        "JobName": "${var.job_name}"
+      },
+      "Type": "Task",
+      "Next": "Hyperparameter Tuning"
+    },
+    "Hyperparameter Tuning": {
+      "Resource": "arn:aws:states:::sagemaker:createHyperParameterTuningJob.sync",
+      "Parameters": {
+        "HyperParameterTuningJobName.$": "$.JobName",
+        "HyperParameterTuningJobConfig": {
+          "Strategy": "Bayesian",
+          "HyperParameterTuningJobObjective": {
+            "Type": "${var.tuning_objective}",
+            "MetricName": "${var.tuning_metric}"
+          },
+          "ResourceLimits": {
+            "MaxNumberOfTrainingJobs": ${tostring(var.max_number_training_jobs)},
+            "MaxParallelTrainingJobs": ${tostring(var.max_parallel_training_jobs)}
+          },
+          "ParameterRanges": ${jsonencode(var.parameter_ranges)}
+        },
+        "TrainingJobDefinition": {
+          "AlgorithmSpecification": {
+            "MetricDefinitions": [
+              {
+                "Name": "${var.tuning_metric}",
+                "Regex": "${var.tuning_metric}: ([0-9\\.]+)"
+              }
+            ],
+            "TrainingImage": "${var.built_in_model_image != null ? var.built_in_model_image : module.ecr_image_byo_model.ecr_image_url_and_tag}",
+            "TrainingInputMode": "File"
+          },
+          "OutputDataConfig": {
+            "S3OutputPath": "s3://${aws_s3_bucket.model_store.id}/models"
+          },
+          "StoppingCondition": {
+            "MaxRuntimeInSeconds": 86400
+          },
+          "ResourceConfig": {
+            "InstanceCount": ${var.training_job_instance_count},
+            "InstanceType": "${var.training_job_instance_type}",
+            "VolumeSizeInGB": ${var.training_job_storage_in_gb}
+          },
+          "RoleArn": "${module.step-functions.iam_role_arn}",
+          "InputDataConfig": [
+            {
+              "ChannelName": "train",
+              "DataSource": {
+                "S3DataSource": {
+                  "S3DataType": "S3Prefix",
+                  "S3Uri": "s3://${aws_s3_bucket.data_store.id}/input_data/train/"
+                  "S3DataDistributionType": "FullyReplicated",
+                }
+              }, 
+              "ContentType": "application/x-recordio",
+              "CompressionType": "None"
+            },
+            {
+              "ChannelName": "validation",
+              "DataSource": {
+                "S3DataSource": {
+                  "S3DataType": "S3Prefix",
+                  "S3Uri": "s3://${aws_s3_bucket.data_store.id}/input_data/validate/"
+                  "S3DataDistributionType": "FullyReplicated",
+                },
+                "ContentType": "application/x-recordio",
+                "CompressionType": "None"
+            },        
+            }
+          ],
+          "StaticHyperParameters": ${jsonencode(var.static_hyperparameters)}
+        }
+      },
+      "Type": "Task",
+      "Next": "Extract Best Model Path"
+    },
+    "Extract Best Model Path": {
+      "Resource": "${module.lambda_functions.function_ids["ExtractModelPath"]}",
+      "ResultPath": "$.BestModelResult",
+      "Type": "Task",
+      "Next": "Query Training Results"
+    },
+    "Query Training Results": {
+      "Resource": "${module.lambda_functions.function_ids["QueryTrainingStatus"]}",
+      "Type": "Task",
+      "Next": "Inference Rule"
+    },
+    "Inference Rule": {
+      "Type": "Choice",
+      "Choices": [
+        {
+          "Variable": "$['trainingMetrics'][0]['Value']",
+          "${var.inference_comparison_operator}": ${var.inference_metric_threshold},
+          "Next": "Save Best Model"
+        }
+      ],
+      "Default": "Model Accuracy Too Low"
+    },
+    "Model Accuracy Too Low": {
+      "Comment": "Validation accuracy lower than threshold",
+      "Type": "Fail"
+    },
+    "Save Best Model": {
+      "Parameters": {
+        "PrimaryContainer": {
+          "Image": "${var.built_in_model_image != null ? var.built_in_model_image : module.ecr_image_byo_model.ecr_image_url_and_tag}",
+          "Environment": {},
+          "ModelDataUrl.$": "$.modelDataUrl"
+        },
+        "ExecutionRoleArn": "${module.step-functions.iam_role_arn}",
+        "ModelName.$": "$.modelName"
+      },
+      "ResultPath": "$.modelSaveResult",
+      "Resource": "arn:aws:states:::sagemaker:createModel",
+      "Type": "Task",
+      "Next": "Load Data from S3 to PostgreSQL"
+    },
+    "Load Data from S3 to PostgreSQL": {
+      "Resource": "${module.lambda_functions.function_ids["LoadDataPostgre"]}",
+      "Type": "Task",
+      "Next": "Monitor Model Performance"
+    }
+    "Monitor Model Performance": {
+      "Resource": "${module.lambda_functions.function_ids["ModelPerformanceMonitor"]}",
+      "Type": "Task",
+      "Next": "Cloud Watch Alarm"
+    },
+    "Cloud Watch Alarm": {
+      "Resource": "${module.lambda_functions.function_ids["CloudWatchAlarm"]}",
+      "Type": "Task",
+      "Next": "${var.endpoint_or_batch_transform}"
+    },
+    ${var.endpoint_or_batch_transform == "Create Model Endpoint Config" ? local.endpoint : local.batch_transform}
+  }
+}
+EOF
+}
+
+
---
+++ catalog/aws/ml-ops-img/variables.tf
@@ -0,0 +1,401 @@
+##############################################
+### Standard variables for all AWS modules ###
+##############################################
+
+variable "name_prefix" {
+  description = "Standard `name_prefix` module input."
+  type        = string
+}
+variable "environment" {
+  description = "Standard `environment` module input."
+  type = object({
+    vpc_id          = string
+    aws_region      = string
+    public_subnets  = list(string)
+    private_subnets = list(string)
+  })
+}
+variable "resource_tags" {
+  description = "Standard `resource_tags` module input."
+  type        = map(string)
+}
+
+########################################
+### Custom variables for this module ###
+########################################
+
+variable "feature_store_override" {
+  description = "Optionally, you can override the default feature store bucket with a bucket that already exists."
+  type        = string
+  default     = null
+}
+
+variable "script_path" {
+  description = "Local path for Glue Python script."
+  type        = string
+  default     = "source/scripts/transform.py"
+}
+
+variable "whl_path" {
+  description = "Local path for Glue Python .whl file."
+  type        = string
+  default     = "source/scripts/python/pandasmodule-0.1-py3-none-any.whl"
+}
+
+variable "train_local_path" {
+  description = "Local path for training data."
+  type        = string
+  default     = "source/data/train/"
+}
+
+variable "score_local_path" {
+  description = "Local path for scoring data. Set to null for endpoint inference"
+  type        = string
+  default     = "source/data/score"
+}
+
+# State Machine input variables
+
+variable "job_name" {
+  description = "Name prefix given to SageMaker model and training/tuning jobs (18 characters or less)."
+  type        = string
+}
+
+variable "endpoint_name" {
+  description = <<EOF
+SageMaker inference endpoint to be created/updated. Endpoint will be created if
+it does not already exist.
+EOF
+  type        = string
+  default     = "training-endpoint"
+}
+
+variable "tuning_objective" {
+  description = "Hyperparameter tuning objective ('Minimize' or 'Maximize')."
+  type        = string
+  default     = "Maximize"
+}
+
+variable "tuning_metric" {
+  description = "Hyperparameter tuning metric, e.g. 'error', 'auc', 'f1', 'accuracy'."
+  type        = string
+  default     = "accuracy"
+}
+
+variable "inference_comparison_operator" {
+  description = <<EOF
+Comparison operator for deploying the trained SageMaker model.
+Used in combination with `inference_metric_threshold`.
+Examples: 'NumericGreaterThan', 'NumericLessThan', etc.
+EOF
+  type        = string
+  default     = "NumericGreaterThan"
+}
+
+variable "inference_metric_threshold" {
+  description = <<EOF
+Threshold for deploying the trained SageMaker model.
+Used in combination with `inference_comparison_operator`.
+EOF
+  type        = number
+  default     = 0.7
+}
+
+variable "endpoint_or_batch_transform" {
+  description = "Choose whether to create/update an inference API endpoint or do batch inference on test data."
+  type        = string
+  default     = "Batch Transform" # Batch Transform or Create Model Endpoint Config
+}
+
+variable "batch_transform_instance_count" {
+  description = "Number of batch transformation instances."
+  type        = number
+  default     = 1
+}
+
+variable "batch_transform_instance_type" {
+  description = "Instance type for batch inference."
+  type        = string
+  default     = "ml.m4.xlarge"
+}
+
+variable "endpoint_instance_count" {
+  description = "Number of initial endpoint instances."
+  type        = number
+  default     = 1
+}
+
+variable "endpoint_instance_type" {
+  description = "Instance type for inference endpoint."
+  type        = string
+  default     = "ml.m4.xlarge"
+}
+
+variable "max_number_training_jobs" {
+  description = "Maximum number of total training jobs for hyperparameter tuning."
+  type        = number
+  default     = 3
+}
+
+variable "max_parallel_training_jobs" {
+  description = "Maximimum number of training jobs running in parallel for hyperparameter tuning."
+  type        = number
+  default     = 1
+}
+
+variable "training_job_instance_count" {
+  description = "Number of instances for training jobs."
+  type        = number
+  default     = 1
+}
+
+variable "training_job_instance_type" {
+  description = "Instance type for training jobs."
+  type        = string
+  default     = "ml.m4.xlarge"
+}
+
+variable "training_job_storage_in_gb" {
+  description = "Instance volume size in GB for training jobs."
+  type        = number
+  default     = 30
+}
+
+variable "static_hyperparameters" {
+  description = <<EOF
+Map of hyperparameter names to static values, which should not be altered during hyperparameter tuning.
+E.g. `{ "kfold_splits" = "5" }`
+EOF
+  type        = map
+  default = {
+  }
+}
+
+variable "parameter_ranges" {
+  description = <<EOF
+Tuning ranges for hyperparameters.
+Expects a map of one or both "ContinuousParameterRanges" and "CategoricalParameterRanges".
+Each item in the map should point to a list of object with the following keys:
+ - Name        - name of the variable to be tuned
+ - MinValue    - min value of the range
+ - MaxValue    - max value of the range
+ - ScalingType - 'Auto', 'Linear', 'Logarithmic', or 'ReverseLogarithmic'
+ - Values      - a list of strings that apply to the categorical paramters
+EOF
+
+  default = {
+    ContinuousParameterRanges = [
+      {
+        Name        = "learning_rate",
+        MinValue    = 0.001,
+        MaxValue    = 0.2,
+        ScalingType = "Auto"
+      },
+      {
+        Name        = "epochs",
+        MinValue    = 5
+        MaxValue    = 40
+        ScalingType = "Auto"
+      },
+      {
+        Name        = "batch-size",
+        MinValue    = 5
+        MaxValue    = 40
+        ScalingType = "Auto"
+      },
+
+    ],
+    CategoricalParameterRanges = [
+      {
+        Name   = "activation",
+        Values = ["sigmoid", "softmax", "relu", "tanh"]
+      },
+      {
+        Name   = "loss_fn",
+        Values = ["categorical_crossentropy", "binary_crossentropy", "categorical_hinge", "hinge"]
+      }
+    ]
+  }
+}
+
+variable "built_in_model_image" {
+  description = <<EOF
+Tuning ranges for hyperparameters.
+Specifying this means that 'bring-your-own' model is not required and the ECR image not created.
+EOF
+  type        = string
+  default     = null
+}
+
+# Model Def variables 
+variable "width" {
+  description = "The width of image file"
+  type        = number
+  default     = 800
+}
+
+variable "height" {
+  description = "The height of image file"
+  type        = number
+  default     = 1200
+}
+
+variable "channels" {
+  description = "The total number of channels of image file"
+  type        = number
+  default     = 3
+}
+
+# ECR input variables
+
+variable "byo_model_image_name" {
+  description = "Image and repo name for bring your own model."
+  type        = string
+  default     = "byo-resnet18"
+}
+
+variable "byo_model_image_source_path" {
+  description = "Local source path for bring your own model docker image."
+  type        = string
+  default     = "source/containers/ml-ops-byo-resnet18"
+}
+
+variable "byo_model_image_tag" {
+  description = "Tag for bring your own model image."
+  type        = string
+  default     = "latest"
+}
+
+# Glue variables
+
+variable "glue_job_name" {
+  description = "Name of the Glue data transformation job name."
+  type        = string
+  default     = "data-transformation"
+}
+
+variable "glue_job_spark_flag" {
+  description = "(Default=True). True to use the default (Spark) Glue job type. False to use Python Shell."
+  type        = string
+  default     = false
+}
+variable "aws_credentials_file" {
+  description = "Path to the AWS credentials file, used to ensure that the correct credentials are used during upload of the ECR image."
+  type        = string
+}
+
+# Cloudwatch alarm variables 
+
+variable "alarm_name" {
+  description = "Name of the cloudwatch alarm"
+  type        = string
+  default     = "Overfitting Alarm"
+}
+
+variable "comparison_operator" {
+  description = <<EOF
+  The arithmetic operation to use when comparing the specified statistic and threshold. The specified statistic value is used as the first operand.
+  Possible values include GreaterThanOrEqualToThreshold, GreaterThanThreshold, LessThanThreshold, LessThanOrEqualToThreshold, LessThanLowerOrGreaterThanUpperThreshold, 
+  LessThanLowerThreshold, and GreaterThanUpperThreshold.
+  EOF
+  type        = string
+  default     = "LessThanOrEqualToThreshold"
+}
+
+variable "eval_period" {
+  description = <<EOF
+  The number of periods over which data is compared to the specified threshold. If you are setting an alarm that requires that a number of consecutive data points 
+  be breaching to trigger the alarm, this value specifies that number. If you are setting an "M out of N" alarm, this value is the N.
+  An alarm's total current evaluation period can be no longer than one day, so this number multiplied by Period cannot be more than 86,400 seconds.
+  EOF
+  type        = number
+  default     = 10
+}
+
+variable "datapoints_to_alarm" {
+  description = <<EOF
+  The number of data points that must be breaching to trigger the alarm. This is used only if you are setting an "M out of N" alarm. In that case, this value is the M.
+  EOF
+  type        = number
+  default     = 10
+}
+
+variable "metric_name" {
+  description = <<EOF
+  The name for the metric associated with the alarm. For each PutMetricAlarm operation, you must specify either MetricName or a Metrics array.
+  If you are creating an alarm based on a math expression, you cannot specify this parameter, or any of the Dimensions , Period , Namespace , Statistic , 
+  or ExtendedStatistic parameters. Instead, you specify all this information in the Metrics array. Values include Training Accuray, Training Loss, 
+  Validation Accuracy, and Validation Loss. 
+  EOF
+  type        = string
+  default     = "Training Accuracy"
+}
+
+variable "period" {
+  description = "The granularity, in seconds, of the returned data points"
+  type        = number
+  default     = 30
+}
+
+variable "statistic" {
+  description = "The statistic to return. It can include any CloudWatch stats or extended stats"
+  type        = string
+  default     = "Maximum"
+}
+
+variable "threshold" {
+  description = "The baseline threshold value that cloudwatch will compare against"
+  type        = number
+  default     = 90.0
+}
+
+variable "actions_enable" {
+  description = "Indicates whether actions should be executed during any changes to the alarm state. "
+  type        = string
+  default     = "True"
+}
+
+variable "alarm_des" {
+  description = "The description for the alarm."
+  type        = string
+  default     = "Alarm when the model is Overfitting"
+}
+
+variable "unit_name" {
+  description = <<EOF
+  The unit of measure for the statistic.You can also specify a unit when you create a custom metric. Units help provide conceptual meaning to your data. 
+  Metric data points that specify a unit of measure, such as Percent, are aggregated separately.
+  If you don't specify Unit , CloudWatch retrieves all unit types that have been published for the metric and attempts to evaluate the alarm. Usually metrics 
+  are published with only one unit, so the alarm will work as intended.
+  However, if the metric is published with multiple types of units and you don't specify a unit, the alarm's behavior is not defined and will behave un-predictably.
+  We recommend omitting Unit so that you don't inadvertently specify an incorrect unit that is not published for this metric. Doing so causes the alarm to be 
+  stuck in the INSUFFICIENT DATA state.
+
+  Possible values: 
+  Seconds, Microseconds, Milliseconds, Bytes, Kilobytes, Megabits, Gigabits, Terabits, Percent, Count, Bytes/Second, Kilobytes/Second, Megabytes/Second, 
+  Gigabytes/Second, Terabytes/Second, Bits/Second, Kilobits/Second, Megabits/Second, Gigabits/Second, Terabits/Second, Count/Second, and None. 
+  EOF
+  type        = string
+  default     = "Percent"
+}
+
+# Data drift monitoring variables 
+
+variable "sample_percent" {
+  description = "The percentage used to sample the input data to perform a data drift detection"
+  type        = number
+  default     = 50
+}
+
+variable "max_timeout_in_sec" {
+  description = "Timeout in seconds. After this amount of time, Amazon SageMaker terminates the job regardless of its current status."
+  type        = number
+  default     = 3600
+}
+
+variable "frequency" {
+  description = "The frequency at which data drift monitoring is performed. Values include: hourly, daily, and daily_every_x_hours (hour_interval, starting_hour)"
+  type        = string
+  default     = "daily"
+}
+
