568368e84fe699b9881292b7d1ff2a63c288936a
AJ Steers <aj.steers@slalom.com>
2020-08-16 00:42:15 -0700

    fix missing paren

--- catalog/aws/singer-taps/outputs.tf
+++ catalog/aws/singer-taps/outputs.tf
@@ -1,6 +1,6 @@
 output "summary" {
   description = "Summary of resources created by this module."
-  value       = <<EOF
+  value = <<EOF
 
 
 Singer Taps Summary:
@@ -16,9 +16,9 @@ Singer Taps Summary:
  NOTE: These CLI actions require setting the profile using a "User Switch Command" from the "Environment" module:
  - Run sync via State Machine:
     ${join("\n   - ", [
-      for sf in module.step_function :
-      "aws stepfunctions start-execution --state-machine-arn ${sf.state_machine_arn}"
-    ]}
+  for sf in module.step_function :
+  "aws stepfunctions start-execution --state-machine-arn ${sf.state_machine_arn}"
+])}
  - Run sync via ECS Task:
    - ${join("\n   - ", module.ecs_tap_sync_task.*.ecs_runtask_cli)}
 
