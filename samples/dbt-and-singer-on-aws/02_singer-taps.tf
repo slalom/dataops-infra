f3fb767873279b53c7b9676345afe0fa3044b497
AJ Steers (CI bot) <aj.steers@slalom.com>
2020-08-16 07:17:41 +0000

    GitOps Bot (Autoformat)

--- samples/dbt-and-singer-on-aws/02_singer-taps.tf
+++ samples/dbt-and-singer-on-aws/02_singer-taps.tf
@@ -18,7 +18,7 @@ module "singer_taps_on_aws" {
     {
       # For 'id', enter any plugin name or alias from the index below, excluding the `tap-` prefix:
       # https://github.com/slalom-ggp/dataops-tools/blob/main/containers/singer/singer_index.yml
-      id = "pardot"
+      id       = "pardot"
       schedule = ["0600"]
       settings = {
         start_date = "2020-02-28T00:00:00Z"
