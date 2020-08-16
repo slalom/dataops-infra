6e46ea788fbdec12a1f66517e7be092e1e5af39c
AJ Steers <aj.steers@slalom.com>
2020-08-15 22:31:54 -0700

    reorder input variables

--- catalog/aws/singer-taps/variables.tf
+++ catalog/aws/singer-taps/variables.tf
@@ -24,48 +24,8 @@ variable "resource_tags" {
 ### Custom variables for this module ###
 ########################################
 
-variable "local_metadata_path" {
-  description = "The local folder which countains tap definitions files: `{tap-name}.rules.txt` and `{tap-name}.plan.yml`"
-  type        = string
-}
-variable "data_lake_type" {
-  description = "Specify `S3` if loading to an S3 data lake, otherwise leave blank."
-  default     = null
-}
-variable "data_lake_metadata_path" {
-  description = <<EOF
-The remote folder for storing tap definitions files.
-Currently only S3 paths (s3://...) are supported.
-EOF
-  type        = string
-}
-variable "data_lake_storage_path" {
-  description = <<EOF
-The path to where files should be stored in the data lake.
-Note:
- - currently only S3 paths (S3://...) are supported.data
- - You must specify `target` or `data_lake_storage_path` but not both.
-EOF
-  type        = string
-  default     = null
-}
-variable "data_file_naming_scheme" {
-  description = <<EOF
-The naming pattern to use when landing new files in the data lake. Allowed variables are:
-`{tap}`, `{table}`, `{version}`, and `{file}`"
-EOF
-  type        = string
-  default     = "{tap}/{table}/v{version}/{file}"
-}
-variable "state_file_naming_scheme" {
-  description = <<EOF
-The naming pattern to use when writing or updating state files. State files keep track of
-data recency and are necessary for incremental loading. Allowed variables are:
-`{tap}`, `{table}`, `{version}`, and `{file}`"
-EOF
-  type        = string
-  default     = "{tap}/{table}/state/{tap}-{table}-v{version}-state.json"
-}
+# Tap and Target Config
+
 variable "taps" {
   description = <<EOF
 A list of objects with the keys `id` (the name of the tap without the 'tap-' prefix),
@@ -91,6 +51,70 @@ EOF
   })
   default = null
 }
+variable "pipeline_version_number" {
+  description = <<EOF
+Optional. (Default="1") Specify a pipeline version number when there are breaking changes which require
+isolation. Note if you want to avoid overlap between versions, be sure to (1) cancel the
+previous version and (2) specify a `start_date` on the new version which is not duplicative
+of the previously covered time period.
+EOF
+  type        = string
+  default     = "1"
+}
+
+# Local Config Files
+
+variable "local_metadata_path" {
+  description = "The local folder which countains tap definitions files: `{tap-name}.rules.txt` and `{tap-name}.plan.yml`"
+  type        = string
+}
+
+# Data Lake Settings
+
+variable "data_lake_type" {
+  description = "Specify `S3` if loading to an S3 data lake, otherwise leave blank."
+  default     = null
+}
+variable "data_lake_metadata_path" {
+  description = <<EOF
+The remote folder for storing tap definitions files.
+Currently only S3 paths (s3://...) are supported.
+EOF
+  type        = string
+}
+variable "data_lake_storage_path" {
+  description = <<EOF
+The path to where files should be stored in the data lake.
+Note:
+ - currently only S3 paths (S3://...) are supported.data
+ - You must specify `target` or `data_lake_storage_path` but not both.
+EOF
+  type        = string
+  default     = null
+}
+
+# File Naming Schemes
+
+variable "data_file_naming_scheme" {
+  description = <<EOF
+The naming pattern to use when landing new files in the data lake. Allowed variables are:
+`{tap}`, `{table}`, `{version}`, and `{file}`"
+EOF
+  type        = string
+  default     = "{tap}/{table}/v{version}/{file}"
+}
+variable "state_file_naming_scheme" {
+  description = <<EOF
+The naming pattern to use when writing or updating state files. State files keep track of
+data recency and are necessary for incremental loading. Allowed variables are:
+`{tap}`, `{table}`, `{version}`, and `{file}`"
+EOF
+  type        = string
+  default     = "{tap}/{table}/state/{tap}-{table}-v{version}-state.json"
+}
+
+# Scheduling and Orchestration
+
 variable "scheduled_sync_times" {
   description = "A list of one or more daily sync times in `HHMM` format. E.g.: `0400` for 4am, `1600` for 4pm"
   type        = list(string)
@@ -103,18 +127,18 @@ Currently the following codes are supported: PST, PDT, EST, UTC
 EOF
   default     = "PT"
 }
-variable "container_image" {
-  description = "Optional. Override the docker image with a custom-managed image."
-  default     = null
+variable "timeout_hours" {
+  description = "Optional. The number of hours before the sync task is canceled and retried."
+  type        = number
+  default     = 48
 }
-variable "container_entrypoint" {
-  description = "Optional. Override the docker image's entrypoint."
-  default     = null
+variable "num_retries" {
+  description = "Optional. The number of retries to attempt if the task fails."
+  default     = 0
 }
-variable "container_command" {
-  description = "Optional. Override the docker image's command."
-  default     = null
-}
+
+# Environment Config
+
 variable "container_num_cores" {
   description = "Optional. Specify the number of cores to use in the container."
   default     = 0.5
@@ -122,30 +146,6 @@ variable "container_num_cores" {
 variable "container_ram_gb" {
   description = "Optional. Specify the amount of RAM to be available to the container."
   default     = 1
-}
-variable "num_retries" {
-  description = "Optional. The number of retries to attempt if the task fails."
-  default     = 0
-}
-variable "timeout_hours" {
-  description = "Optional. The number of hours before the sync task is canceled and retried."
-  type        = number
-  default     = 48
-}
-variable "pipeline_version_number" {
-  description = <<EOF
-Optional. (Default="1") Specify a pipeline version number when there are breaking changes which require
-isolation. Note if you want to avoid overlap between versions, be sure to (1) cancel the
-previous version and (2) specify a `start_date` on the new version which is not duplicative
-of the previously covered time period.
-EOF
-  type        = string
-  default     = "1"
-}
-variable "container_args" {
-  type        = list(string)
-  description = "Optional. A list of additional args to send to the container."
-  default     = ["--config_file=False", "--target_config_file=False"]
 }
 variable "use_private_subnet" {
   description = <<EOF
@@ -155,4 +155,24 @@ not require a NAT gateway.
 EOF
   type        = bool
   default     = false
+}
+
+# Advanced Settings (Optional)
+
+variable "container_image" {
+  description = "Optional. Override the docker image with a custom-managed image."
+  default     = null
+}
+variable "container_command" {
+  description = "Optional. Override the docker image's command."
+  default     = null
+}
+variable "container_args" {
+  type        = list(string)
+  description = "Optional. A list of additional args to send to the container."
+  default     = ["--config_file=False", "--target_config_file=False"]
+}
+variable "container_entrypoint" {
+  description = "Optional. Override the docker image's entrypoint."
+  default     = null
 }
