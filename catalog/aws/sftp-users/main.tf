3ec004ccb32801837562ad29aba59caecf016ea6
AJ Steers <aj.steers@slalom.com>
2020-08-18 08:23:11 -0700

    cloudposse now supports 0.13

--- catalog/aws/sftp-users/main.tf
+++ catalog/aws/sftp-users/main.tf
@@ -34,9 +34,7 @@ resource "aws_transfer_ssh_key" "new_user_ssh_keys" {
 
 module "ssh_key_pair" {
   for_each = var.users
-  # TODO: Pending PR on cloudposse upstream repo: https://github.com/cloudposse/terraform-aws-key-pair/pull/49
-  # source              = "git::https://github.com/cloudposse/terraform-aws-key-pair.git?ref=master"
-  source                = "git::https://github.com/aaronsteers/terraform-aws-key-pair.git?ref=main"
+  source                = "git::https://github.com/cloudposse/terraform-aws-key-pair.git?ref=master"
   namespace             = var.name_prefix
   stage                 = "stfp"
   name                  = each.value
