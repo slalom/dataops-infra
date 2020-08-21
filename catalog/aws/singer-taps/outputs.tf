ed98130a991bb703c2067440111e72e24e8507bf
AJ Steers <aj.steers@slalom.com>
2020-08-21 15:03:13 -0700

    update singer outputs

--- catalog/aws/singer-taps/outputs.tf
+++ catalog/aws/singer-taps/outputs.tf
@@ -5,16 +5,12 @@ output "summary" {
 
 Singer Taps Summary:
 
- - ECS Tasks URL: https://console.aws.amazon.com/ecs/home?region=${var.environment.aws_region}#/clusters/${module.ecs_cluster.ecs_cluster_name}/tasks
  - Dashboard URL: https://console.aws.amazon.com/cloudwatch/home?region=${var.environment.aws_region}#dashboards:name=${aws_cloudwatch_dashboard.main.dashboard_name}
- - Logging URL:   ${module.ecs_tap_sync_task.ecs_logging_url}
  - State Machine: ${module.step_function.state_machine_name}
 
- NOTE: These CLI actions require setting the profile using a "User Switch Command" from the "Environment" module:
+ NOTE: Running from CLI require setting the profile using a "User Switch Command" from the "Environment" module:
  - Run sync via State Machine:
      aws stepfunctions start-execution --state-machine-arn ${module.step_function.state_machine_arn}
- - Run sync via ECS Task:
-     ${module.ecs_tap_sync_task.ecs_runtask_cli}
 
 EOF
 }
