3febf9cdb410f8d58ced4d7458617b31e2960de7
AJ Steers <aj.steers@slalom.com>
2020-08-24 09:11:25 -0700

    resolve TODO items

--- components/aws/vpc/main.tf
+++ components/aws/vpc/main.tf
@@ -24,15 +24,6 @@ locals {
   )
 }
 
-#TODO: Remove this old version after all legacy dependencies have cleared.
-provider "aws" {
-  alias                   = "regional" # used for region-specific AZ lookup
-  version                 = "~> 2.10"
-  region                  = local.aws_region
-  shared_credentials_file = var.aws_credentials_file
-  profile                 = var.aws_profile
-}
-
 provider "aws" {
   alias                   = "region_lookup" # used for region-specific AZ lookup
   version                 = "~> 2.10"
