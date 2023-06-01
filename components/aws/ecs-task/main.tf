302d16a01c3431607470dbd356bd31d2db93de63
Mikey Curtis <mikey.curtis@slalom.com>
2023-06-01 11:19:32 -0700

    Updated node version

--- components/aws/ecs-task/main.tf
+++ components/aws/ecs-task/main.tf
@@ -236,7 +236,7 @@ resource "aws_lambda_function" "lambda_kinesis_firehose_data_transformation" {
   role             = aws_iam_role.lambda[0].arn
   handler          = "index.handler"
   source_code_hash = data.archive_file.kinesis_firehose_data_transformation[0].output_base64sha256
-  runtime          = "nodejs12.x"
+  runtime          = "nodejs18.x"
   timeout          = 60
   tags             = var.resource_tags
 
